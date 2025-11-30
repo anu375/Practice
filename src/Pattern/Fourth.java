90de52c092ee0a874502b7e590ca6c6ee439d010
Anushka Jadhav <anujadhav32004@gmail.com>
2025-11-30 21:58:48 +0530

    first commit

---
+++ src/Pattern/Fourth.java
@@ -0,0 +1,20 @@
+package Pattern;
+
+public class Fourth {
+
+	public static void main(String[] args) {
+	
+		int n = 4;
+		int k = 1;
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1; j<=n; j++)
+			{
+				System.out.printf("%2d",k);
+				System.out.print("  ");
+				k++;
+			}
+			System.out.println();
+		}
+	}
+}
