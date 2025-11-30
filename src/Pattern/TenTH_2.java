d695a7b6977f68ca1c835c980a7a6099a67f27df
Anushka Jadhav <anujadhav32004@gmail.com>
2025-11-30 22:28:35 +0530

    first commit

---
+++ src/Pattern/TenTH_2.java
@@ -0,0 +1,20 @@
+package Pattern;
+
+public class TenTH_2 
+{
+
+	public static void main(String[] args) 
+	{
+		char i;
+		char j;
+		for(i='A'; i<='D'; i++)
+		{
+			for(j='A'; j<='D';j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
