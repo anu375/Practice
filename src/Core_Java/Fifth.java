8239b22642ae35dbdd047f8479406d877082aa77
Anushka Jadhav <anujadhav32004@gmail.com>
2026-01-23 00:10:09 +0530

    Palindrome : Find

---
+++ src/Core_Java/Fifth.java
@@ -0,0 +1,22 @@
+package Core_Java;
+
+public class Fifth 
+{
+
+	public static void main(String[] args)
+	{
+		int[] arr = {10, 45, 2, 89, 32};
+		int max = arr[0];
+		
+		for(int i = 1; i < arr.length; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+			}
+			
+			System.out.println("Largest : "+max);
+		}
+	}
+	
+}
---
+++ src/Core_Java/Fourth.java
@@ -0,0 +1,29 @@
+package Core_Java;
+
+/**
+ * palindrome
+ */
+public class Fourth
+{
+	public static void main(String[] args)
+	{
+
+		String str = "madam";
+		String rev = " ";
+	
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			rev += str.charAt(i);
+		}
+		
+		if(str.equals(rev))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
+	
+}
