45ce6ba4a890be3440e43021c7d154aeed115218
Anushka Jadhav <anujadhav32004@gmail.com>
2026-01-23 12:50:42 +0530

    WordCount

---
+++ src/Core_Java/WordCount.java
@@ -0,0 +1,15 @@
+package Core_Java;
+
+
+public class WordCount
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Java is very powerful language";
+		String[] words = str.split(" ");
+		System.out.println("Word Count : "+words.length);
+	}
+	
+	
+}
