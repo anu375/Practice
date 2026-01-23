34080235d6e0eb13645dcadcac89c6c1df7c196a
Anushka Jadhav <anujadhav32004@gmail.com>
2026-01-23 12:33:09 +0530

    Largest No : Find

--- src/Core_Java/Fifth.java
+++ src/Core_Java/Fifth.java
@@ -1,22 +1,32 @@
 package Core_Java;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * find largest number in array
+ */
+
 public class Fifth 
 {
 
 	public static void main(String[] args)
 	{
-		int[] arr = {10, 45, 2, 89, 32};
-		int max = arr[0];
 		
-		for(int i = 1; i < arr.length; i++)
-		{
-			if(arr[i] > max)
-			{
-				max = arr[i];
-			}
-			
-			System.out.println("Largest : "+max);
-		}
+		Integer[] arr = {10, 45, 2, 99, 23,99};
+
+		int max = Collections.max(Arrays.asList(arr));
+
+		System.out.println("Largest number: " + max);
+
+		
+//		int[] arr = {10, 45, 2, 99, 23,101};
+//
+//		int max = Arrays.stream(arr).max().getAsInt();
+//
+//		System.out.println("Largest number: " + max);
+
+		
 	}
 	
 }
