32897c7517b5414dcd7531d8cce472f1800976e9
Anushka Jadhav <anujadhav32004@gmail.com>
2026-01-24 14:35:24 +0530

    ArrayList :::

---
+++ src/Core_Java/Student_10.java
@@ -0,0 +1,23 @@
+package Core_Java;
+
+import java.util.ArrayList;
+
+class Student_10 
+{
+	    
+	public static void main(String[] args) 
+	{
+	
+		ArrayList<Integer>list = new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		
+		for(Integer i : list)
+		{
+			System.out.println(i+" ");
+		}
+		
+	}
+	
+}
