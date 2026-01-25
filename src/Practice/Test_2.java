de71656dba0e941a695be8a5e28bce3619381df7
anu <anujadhav32004@gmail.com>
2026-01-25 12:16:33 +0530

    LargestOf_3NO. ::

---
+++ src/Practice/Test_2.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Test_2 
+{
+
+	public static void main(String[] args)
+	{
+		int a = 90;
+		int b = 40;
+		int c = 90;
+		
+		System.out.println(Math.max(a, Math.max(b, c)));
+	}
+	
+}
